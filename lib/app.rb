a4793e176a73de93cb28dbeb4cdec5e8cf77d0f2
Alison Woodman <aliwoodman@gmail.com>
2017-07-20 15:32:46 +0100

    Controller made skinnier

--- lib/app.rb
+++ lib/app.rb
@@ -9,27 +9,22 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
-    $game = Game.new
+    $game = Game.new(Player.new(params[:player_1_name]),Player.new(params[:player_2_name]))
     redirect '/play'
   end
 
   get '/play' do
-    $player_1_hp, $player_2_hp = $player_1.hp, $player_2.hp
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
     last_move = session[:last_move]
     attack_confirmation = ''
     if last_move == 'Attack'
-      attack_confirmation = "#{@player_1_name} has attacked #{@player_2_name}"
+      attack_confirmation = "#{$game.player_1.name} has attacked #{$game.player_2.name}"
     end
     erb :play, { locals: { attack_confirmation: attack_confirmation } }
   end
 
   post '/attacked' do
     session[:last_move] = params[:attack]
-    $game.attack($player_2)
+    $game.attack($game.player_2)
     redirect '/play'
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,26 @@
 require 'game'
 describe Game do
-  subject(:game) {described_class.new}
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
+  subject(:game) {described_class.new(player_1, player_2)}
   let(:player) {Player.new('Gerrald')}
+
+
+  describe '#initialize' do
+    it 'should have two players' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
   describe '#attack' do
+
     it 'should reduce hit points' do
       expect{ game.attack(player) }.to change{player.hp}.by(-10)
+    end
+
+    it 'damages the player' do
+      expect(player_2).to receive(:attack_reduce)
+      game.attack(player_2)
     end
   end
 end
